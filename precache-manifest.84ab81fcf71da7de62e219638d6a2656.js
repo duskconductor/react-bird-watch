737f736ae96297f7655299da31c30053b2550c9c
Mark <markafullton@outlook.com>
2020-09-08 16:37:24 -0700

    Updates

---
+++ precache-manifest.84ab81fcf71da7de62e219638d6a2656.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912a27a38438fef921c242b1a7c485f4",
+    "url": "/react-bird-watch/index.html"
+  },
+  {
+    "revision": "26ce8413bc2566a90da8",
+    "url": "/react-bird-watch/static/js/2.a53f85b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bird-watch/static/js/2.a53f85b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df53d2b85d47c60551c0",
+    "url": "/react-bird-watch/static/js/main.6170b2bd.chunk.js"
+  },
+  {
+    "revision": "acb963ca554a4311f8ef",
+    "url": "/react-bird-watch/static/js/runtime-main.09775564.js"
+  }
+]);
